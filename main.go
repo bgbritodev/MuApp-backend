91e0c21e101a571b08b9865f9f04f3c2ef464023
bgbritodev <bgbritodev@gmail.com>
2024-08-11 11:53:35 -0300

    Set up data base connection and folders

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/bgbritodev/MuApp-backend/config"
 )
 
 func main() {
+	config.Connect()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, MuApp-backend!")
 	})
